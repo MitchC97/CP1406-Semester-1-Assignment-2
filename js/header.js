076ed8becd8d44fd8a7062c4bf565fa0ccb5958b
yvan-burrie <yvan.burrie@hotmail.com>
2017-05-30 13:49:37 +1000

    General Changes

--- js/header.js
+++ js/header.js
@@ -2,12 +2,16 @@
 $(window).scroll(function(){
 	var h = 60; // height of scroll
 	var t = 200; // time of fading
+	var s1 = 120; // stretch 1
+	var s2 = 220; // stretch 2
 	if(window.pageYOffset >= h){
+		$('#header_gap').height(s1);
 		$('#header_banner').fadeOut(t,function(){
 			$('#header_banner').css('display','none');
 		});
 	}
 	if(window.pageYOffset < h){
+		$('#header_gap').height(s2);
 		$('#header_banner').fadeIn(t,function(){
 			$('#header_banner').css('display','block');
 		});
